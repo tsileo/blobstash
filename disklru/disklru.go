1f7143e96b7ef2d6f58f8b8e08436980a2723f4a
Thomas Sileo <thomas.sileo@gmail.com>
2014-05-11 17:00:40 +0200

    Added disklru package to replace in-memory LRU

---
+++ disklru/disklru.go
@@ -0,0 +1,269 @@
+/*
+
+Package disklru implements a disk-based LRU cache.
+
+A small key-value store ((powered by kv [1])) keeps track of the keys/size/last access time of each time, and data are stored in files. 
+Eviction is triggered when the cache size exceed a limit,
+it tries to keep the size of the cache under a fixed threshold and remove lest recently used item first.
+
+Index
+
+Data are stored the following way (all the uint32 are binary encoded):
+
+- Index byte + last access time (uint32) + key => size (uint32)
+- Keys byte + key => current access time
+- MetaCnt byte => number of items currently in the cache (uint32)
+- MetaSize byte => total size of the cache (uint32)
+
+Links
+
+	[1] https://github.com/cznic/kv
+
+*/
+package disklru
+
+import (
+	"sync"
+	"os"
+	"io"
+	"io/ioutil"
+	"bytes"
+	"path/filepath"
+	"encoding/binary"
+	"time"
+	
+	"github.com/cznic/kv"
+)
+
+// Namespaces for the DB keys
+const (
+	Keys byte = iota
+	Index
+	MetaSize
+	MetaCnt
+)
+
+func opts() *kv.Options {
+	return &kv.Options{
+		VerifyDbBeforeOpen:  true,
+		VerifyDbAfterOpen:   true,
+		VerifyDbBeforeClose: true,
+		VerifyDbAfterClose:  true,
+	}
+}
+
+type DiskLRU struct {
+	Func func(string) []byte
+	Threshold int64
+	db          *kv.DB
+	path string
+	sync.Mutex
+}
+
+type CacheItem struct {
+	Key string
+	Index uint32
+	Size uint32
+}
+
+// New initialize a new DiskLRU.
+func New(path string, f func(string) []byte, threshold int64) (*DiskLRU, error) {
+	db_path := filepath.Join(path, "db")
+	if err := os.MkdirAll(path, 0700); err != nil {
+		return nil, err
+	}
+	createOpen := kv.Open
+	if _, err := os.Stat(db_path); os.IsNotExist(err) {
+		createOpen = kv.Create
+	}
+	db, err := createOpen(db_path, opts())
+	return &DiskLRU{f, threshold, db, path, sync.Mutex{}}, err
+}
+
+// Build the key by adding the given byte to namespace keys in the DB.
+func buildKey(ktype byte, key string) []byte {
+	bkey := []byte(key)
+	out := make([]byte, len(bkey)+1)
+	out[0] = ktype
+	copy(out[1:], bkey)
+	return out
+}
+
+func indexKey(key string, index uint32) []byte {
+	bkey := []byte(key)
+	out := make([]byte, len(bkey)+5)
+	out[0] = Index
+	binary.BigEndian.PutUint32(out[1:5], index)
+	copy(out[5:], bkey)
+	return out
+}
+
+func decodeIndexKey(key []byte) (string, uint32) {
+	return string(key[5:]), binary.BigEndian.Uint32(key[1:5])
+}
+
+// Get the value for the given key, call Func if the key isn't stored yet.
+func (lru *DiskLRU) Get(key string) (data []byte, fetched bool, err error) {
+	lru.Lock()
+	defer func() {
+		lru.Unlock()
+		defer lru.evict()
+	}()
+	accessTime := time.Now().UTC().Unix()
+	bkey := buildKey(Keys, key)
+	lastAccessTime, err := lru.getUint32(bkey)
+	if err != nil {
+		return
+	}
+	if lastAccessTime == 0 {
+		fetched = true
+		data = lru.Func(key)
+		if err = ioutil.WriteFile(filepath.Join(lru.path, key), data, 0644); err != nil {
+			return
+		}
+		// Increments the internal counter (for size and items count)
+		if err = lru.incrUint32([]byte{MetaSize}, len(data)); err != nil {
+			return
+		}
+		if err = lru.incrUint32([]byte{MetaCnt}, 1); err != nil {
+			return
+		}
+	} else {
+		// Remove the precedent last access time
+		data, err = ioutil.ReadFile(filepath.Join(lru.path, key))
+		if err != nil {
+			return
+		}
+		// Remove the precedent index entry
+		if err = lru.db.Delete(indexKey(key, lastAccessTime)); err != nil {
+			return
+		}
+	}
+	// Update the last access time
+	nikey := indexKey(key, uint32(accessTime))
+	if err = lru.putUint32(nikey, uint32(len(data))); err != nil {
+		return
+	}
+	// Update the Keys index access time
+	if err = lru.putUint32(bkey, uint32(accessTime)); err != nil {
+		return
+	}
+	return
+}
+
+// remove remove an time from the cache
+// (it doesn't decrement internal counter about size/cnt).
+func (lru *DiskLRU) remove(key string) (err error) {
+	bkey := buildKey(Keys, key)
+	lastAccessTime, err := lru.getUint32(bkey)
+	if err != nil {
+		return
+	}
+	if lastAccessTime != 0 {
+		ikey := indexKey(key, lastAccessTime)
+		if err = lru.db.Delete(ikey); err != nil {
+			return
+		}
+		if err = lru.db.Delete(bkey); err != nil {
+			return
+		}
+	}
+	return nil
+}
+
+// Size returns the total size of data stored to disk.
+func (lru *DiskLRU) Size() uint32 {
+	lru.Lock()
+	defer lru.Unlock()
+	skey := []byte{MetaSize}
+	tsize, _ := lru.getUint32(skey)
+	return tsize
+}
+
+// Cnt returns the number of items in the cache.
+func (lru *DiskLRU) Cnt() uint32 {
+	lru.Lock()
+	defer lru.Unlock()
+	skey := []byte{MetaCnt}
+	cnt, _ := lru.getUint32(skey)
+	return cnt
+}
+
+// evict evicts least recently items until the total size is under the threshold.
+func (lru *DiskLRU) evict() error {
+	skey := []byte{MetaSize}
+	tsize, err := lru.getUint32(skey)
+	if err != nil {
+		return err
+	}
+	if int64(tsize) > lru.Threshold {
+		items := make(chan *CacheItem)
+		go lru.Iter(items)
+		csize := int64(tsize)
+		for item := range items {
+			// The cache must contains at list one element, even above the threshold
+			if csize > lru.Threshold {
+				if err := lru.remove(item.Key); err != nil {
+					return err
+				}
+				lru.incrUint32([]byte{MetaSize}, -int(item.Size))
+				lru.incrUint32([]byte{MetaCnt}, -1)
+				csize -= int64(item.Size)
+			}
+		}
+	}
+	return nil
+}
+
+// Iter iterate over keys and index (unix timestamp of last access).
+func (lru *DiskLRU) Iter(items chan<- *CacheItem) {
+	lru.Lock()
+	defer lru.Unlock()
+	enum, _, _ := lru.db.Seek(buildKey(Index, ""))
+	endBytes := buildKey(Index, "\xff")
+	for {
+		k, v, err := enum.Next()
+		if err == io.EOF || bytes.Compare(k, endBytes) > 0 {
+			break
+		}
+		rkey, rindex := decodeIndexKey(k)
+		rsize := binary.LittleEndian.Uint32(v)
+		items <- &CacheItem{Key:rkey, Index:rindex, Size:rsize}
+	}
+	close(items)
+}
+
+// Store a uint32 as binary data.
+func (lru *DiskLRU) putUint32(key []byte, value uint32) error {
+	val := make([]byte, 4)
+	binary.LittleEndian.PutUint32(val[:], value)
+	err := lru.db.Set(key, val)
+	return err
+}
+
+// Retrieve a binary encoded uint32.
+func (lru *DiskLRU) getUint32(key []byte) (uint32, error) {
+	data, err := lru.db.Get(nil, key)
+	if err != nil || data == nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint32(data), nil
+}
+
+// Increment a binary encoded uint32.
+func (lru *DiskLRU) incrUint32(key []byte, step int) error {
+	data, err := lru.db.Get(nil, key)
+	var value uint32
+	if err != nil {
+		return err
+	}
+	if data == nil {
+		value = 0
+	} else {
+		value = binary.LittleEndian.Uint32(data)
+	}
+	val := make([]byte, 4)
+	binary.LittleEndian.PutUint32(val[:], value+uint32(step))
+	err = lru.db.Set(key, val)
+	return err
+}
---
+++ disklru/disklru_test.go
@@ -0,0 +1,63 @@
+package disklru
+
+import (
+	"testing"
+	"log"
+	"os"
+	"bytes"
+	"time"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func LRUTestFunc(key string) []byte {
+	return []byte(key)
+}
+
+var fakeData = []struct{
+	key string
+	data []byte
+	fetched bool
+	size uint32
+	cnt uint32
+}{
+	{"testget", []byte("testget"), true, 7, 1},
+	{"testget", []byte("testget"), false, 7, 1},
+	{"testget2", []byte("testget2"), true, 15, 2},
+	{"aaaaaaaaaaaaaaaaaaaa", []byte("aaaaaaaaaaaaaaaaaaaa"), true, 35, 3},
+	{"bbbbbbbbbbbbbbbbbbbb", []byte("bbbbbbbbbbbbbbbbbbbb"), true, 48, 3},
+	{"aaaaaaaaaaaaaaaaaaaa", []byte("aaaaaaaaaaaaaaaaaaaa"), false, 48, 3},
+	{"cccccccccccccccccccc", []byte("cccccccccccccccccccc"), true, 40, 2},
+	{"bbbbbbbbbbbbbbbbbbbb", []byte("bbbbbbbbbbbbbbbbbbbb"), false, 40, 2},
+	{"cccccccccccccccccccc", []byte("cccccccccccccccccccc"), false, 40, 2},
+	{"testget", []byte("testget"), true, 47, 3},
+}
+
+func TestDiskLRU(t *testing.T) {
+	lru, err := New("tmp_lru_test", LRUTestFunc, 50)
+	check(err)
+	defer os.RemoveAll("tmp_lru_test")
+
+	for _, tdata := range fakeData {
+		data, fetched, err := lru.Get(tdata.key)
+		check(err)
+		if !bytes.Equal(data, tdata.data) {
+			t.Errorf("Bad get result, got:%+v, expected:%+v", data, tdata.data)
+		}
+		if fetched != tdata.fetched {
+			t.Errorf("Bad get result, fetched=%v, expected fetched=%+v", fetched, tdata)
+		}
+		if lru.Size() != tdata.size {
+			t.Errorf("Bad DiskLRU size, got:%v, expected:%v", lru.Size(), tdata.size)
+		}
+		if lru.Cnt() != tdata.cnt {
+			t.Errorf("Bad DiskLRU items count, got:%v, expected:%v", lru.Cnt(), tdata.cnt)
+		}
+		time.Sleep(500 * time.Millisecond)
+	}	
+
+}
