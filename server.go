988d20cdc65aaa9b7be9b87b25a68f6cb47c762f
Thomas Sileo <thomas.sileo@gmail.com>
2014-06-01 17:33:20 +0200

    Backends are now defined by config file

--- server.go
+++ server.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
-	"github.com/tsileo/datadatabase/server"
-	
+
 	"github.com/bitly/go-simplejson"
 
+	"github.com/tsileo/datadatabase/server"
 	"github.com/tsileo/datadatabase/config"
-	"github.com/tsileo/datadatabase/backend/blobsfile"
 	)
 
 func main() {
 	stop := make(chan bool)
 
 	//keyPath := "/home/thomas/genkey/datadb_key"
-	dat, _ := ioutil.ReadFile("config.json")
-	conf, _ := simplejson.NewJson(dat)
-	blobBackend := config.NewFromConfig(conf)
-	metaBackend := blobsfile.New("/box/blobstoremetas")
+	dat, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(fmt.Errorf("can't read config file: %v", err))
+	}
+	conf, err := simplejson.NewJson(dat)
+	if err != nil {
+		panic(fmt.Errorf("can't decode config file: %v", err))
+	}
+	_, exists := conf.CheckGet("backends")
+	if !exists {
+		panic(fmt.Errorf("missing top-level key \"backends\" from config file"))
+	}
+	blobBackend := config.NewFromConfig(conf.GetPath("backends", "blobs"))
+	metaBackend := config.NewFromConfig(conf.GetPath("backends", "meta"))
 	server.New("127.0.0.1:9736", "./tmp_db", blobBackend, metaBackend, stop)
 }
