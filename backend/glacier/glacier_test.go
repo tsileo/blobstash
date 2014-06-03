695593b2dfe9bc63d99a8d887822db6aed3042cb
Thomas Sileo <thomas.sileo@gmail.com>
2014-06-03 11:19:59 +0200

    Fixed tests relying on BlobsFile

--- backend/glacier/glacier_test.go
+++ backend/glacier/glacier_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestGlacierBackend(t *testing.T) {
-	dest := blobsfile.New("tmp_blobsfile_1", false, false)
+	dest := blobsfile.New("tmp_blobsfile_1", 0, false, false)
 	defer os.RemoveAll("tmp_blobsfile_1")
 	b := New(dest)
 	backend.TestWriteOnly(t, b)
--- backend/mirror/mirror_test.go
+++ backend/mirror/mirror_test.go
@@ -9,9 +9,9 @@ import (
 )
 
 func TestMirrorBackend(t *testing.T) {
-	dest1 := blobsfile.New("tmp_blobsfile_1", false, false)
+	dest1 := blobsfile.New("tmp_blobsfile_1", 0, false, false)
 	defer os.RemoveAll("tmp_blobsfile_1")
-	dest2 := blobsfile.New("tmp_blobsfile_2", false, false)
+	dest2 := blobsfile.New("tmp_blobsfile_2", 0, false, false)
 	defer os.RemoveAll("tmp_blobsfile_2")
 	b := New(dest1, dest2)
 	backend.Test(t, b)
