e02ad591ce2e20cda75df3505b135b9a357cf4bd
Thomas Sileo <thomas.sileo@gmail.com>
2015-01-18 12:25:51 +0100

    api: working vkv endpoint

--- api/api.go
+++ api/api.go
@@ -6,8 +6,11 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/tsileo/blobstash/vkv"
 )
 
 func WriteJSON(w http.ResponseWriter, data interface{}) {
@@ -19,20 +22,23 @@ func WriteJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
 }
-
-type VkvItem struct {
-	Key     string `json:"key,omitempty"`
-	Value   string `json:"value"`
-	Version int    `json:"version"`
-}
-
-func vkvHandler() func(http.ResponseWriter, *http.Request) {
+func vkvHandler(db *vkv.DB) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
 			vars := mux.Vars(r)
-			res := &VkvItem{
-				Key: vars["key"],
+			iversion := -1
+			version := r.URL.Query().Get("version")
+			if version != "" {
+				iver, err := strconv.Atoi(version)
+				if err != nil {
+					panic(err)
+				}
+				iversion = iver
+			}
+			res, err := db.Get(vars["key"], iversion)
+			if err != nil {
+				panic(err)
 			}
 			WriteJSON(w, res)
 		case "HEAD":
@@ -46,9 +52,9 @@ func vkvHandler() func(http.ResponseWriter, *http.Request) {
 			vars := mux.Vars(r)
 			k := vars["key"]
 			v := r.FormValue("value")
-			res := &VkvItem{
-				Key:   k,
-				Value: v,
+			res, err := db.Put(k, v, -1)
+			if err != nil {
+				panic(err)
 			}
 			WriteJSON(w, res)
 		default:
@@ -57,19 +63,14 @@ func vkvHandler() func(http.ResponseWriter, *http.Request) {
 	}
 }
 
-type VersionsReponse struct {
-	Key      string     `json:"key"`
-	Versions []*VkvItem `json:"versions"`
-}
-
-func vkvVersionsHandler() func(http.ResponseWriter, *http.Request) {
+func vkvVersionsHandler(db *vkv.DB) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
 			vars := mux.Vars(r)
-			res := &VersionsReponse{
-				Key:      vars["key"],
-				Versions: []*VkvItem{},
+			res, err := db.Versions(vars["key"], 0, int(time.Now().UTC().UnixNano()), 0)
+			if err != nil {
+				panic(err)
 			}
 			WriteJSON(w, res)
 			return
@@ -80,9 +81,14 @@ func vkvVersionsHandler() func(http.ResponseWriter, *http.Request) {
 }
 
 func main() {
+	db, err := vkv.New("devdb")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/vkv/{key}", vkvHandler())
-	r.HandleFunc("/api/v1/vkv/{key}/versions", vkvVersionsHandler())
+	r.HandleFunc("/api/v1/vkv/{key}", vkvHandler(db))
+	r.HandleFunc("/api/v1/vkv/{key}/versions", vkvVersionsHandler(db))
 	http.Handle("/", r)
 	http.ListenAndServe(":8050", nil)
 }
