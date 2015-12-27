fcabce7b668352f80c31831ae305b41f6abfa28d
Thomas Sileo <thomas.sileo@gmail.com>
2015-12-27 20:20:16 +0100

    ext/lua: new bewit pkg

---
+++ bewit/bewit.go
@@ -0,0 +1,51 @@
+package bewit
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/tent/hawk-go"
+)
+
+// FIXME(tsileo) generate/store a secure key
+var key = "mysuperkey"
+var appID = "luascripts"
+
+// Since we only use hawk bewit auth, there's no nonce handling for us
+func nonceCheckFunc(nonce string, t time.Time, cred *hawk.Credentials) bool {
+	panic("should never be called")
+	return true
+
+}
+
+func credentialsLookupFunc(cred *hawk.Credentials) error {
+	cred.Hash = sha256.New
+	cred.Key = key
+	return nil
+}
+
+// New returns a `bewit` token valid for the given dealy
+func New(url string, delay time.Duration) (string, error) {
+	auth, err := hawk.NewURLAuth(url, &hawk.Credentials{
+		ID:   appID,
+		Key:  key,
+		Hash: sha256.New,
+	}, delay)
+	fmt.Printf("hawkAuth=%+v\n", auth)
+	if err != nil {
+		return "", err
+	}
+	return auth.Bewit(), nil
+}
+
+// Check will try to authenticate the `bewit` parameter for the given request
+func Check(r *http.Request) error {
+	hawkAuth, err := hawk.NewAuthFromRequest(r, credentialsLookupFunc, nonceCheckFunc)
+	fmt.Printf("hawkAuth=%+v\n", hawkAuth)
+	if err != nil {
+		return err
+	}
+	return hawkAuth.Valid()
+}
