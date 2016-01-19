5251baa404c4e908c1764a4bdadc2d23f41c8658
Thomas Sileo <thomas.sileo@gmail.com>
2016-01-19 21:19:46 +0100

    cmd/blob: started a new CLI tool
    `blob` is a basic CLI with two features:
     - upload a blob from STDIN and return the hash
     - Output raw blob content for the given hash

---
+++ cmd/blob/blob.go
@@ -0,0 +1,52 @@
+/*
+
+blob is a tiny command line too to interact with BlobStash blob store.
+
+ - read from STDIN, upload and output hash
+ - output raw blob from hash
+
+ That's it!
+
+*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/tsileo/blobstash/client"
+
+	"github.com/dchest/blake2b"
+)
+
+func main() {
+	// TODO(tsileo): host flag
+	// and display an usage func
+	// api key as a venv
+	apiKey := os.Getenv("BLOB_API_KEY")
+	if len(os.Args) < 2 {
+		fmt.Println("Missing hash, use \"-\" to upload from stdin")
+		os.Exit(1)
+	}
+	blobstore := client.NewBlobStore("")
+	blobstore.SetAPIKey(apiKey)
+	if os.Args[1] == "-" {
+		stdin, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		hash := fmt.Sprintf("%x", blake2b.Sum256(stdin))
+		if err := blobstore.Put(hash, stdin); err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s", hash)
+		os.Exit(0)
+	}
+	blob, err := blobstore.Get(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", blob)
+	os.Exit(0)
+}
