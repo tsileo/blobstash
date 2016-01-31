c1c14b96e062996445603f86702692033f627faf
Thomas Sileo <thomas.sileo@gmail.com>
2016-01-31 14:34:48 +0100

    ext/filetree: import file/dir writer

---
+++ ext/filetree/filetreeutil/meta/meta.go
@@ -0,0 +1,103 @@
+package meta
+
+import (
+	"encoding/json"
+	"fmt"
+	"mime"
+	"path/filepath"
+	"sync"
+
+	"github.com/dchest/blake2b"
+)
+
+var metaPool = sync.Pool{
+	New: func() interface{} {
+		return &Meta{
+			Refs:    []interface{}{},
+			Version: "1",
+		}
+	},
+}
+
+type Meta struct {
+	Name    string                 `json:"name"`
+	Type    string                 `json:"type"`
+	Size    int                    `json:"size"`
+	Mode    uint32                 `json:"mode"`
+	ModTime string                 `json:"mtime"`
+	Refs    []interface{}          `json:"refs"`
+	Version string                 `json:"version"`
+	Extra   map[string]interface{} `json:"extra,omitempty"`
+	Hash    string                 `json:"-"`
+}
+
+func (m *Meta) free() {
+	m.Refs = m.Refs[:0]
+	m.Name = ""
+	m.Extra = nil
+	m.Type = ""
+	m.Size = 0
+	m.Mode = 0
+	m.ModTime = ""
+	m.Hash = ""
+	m.Version = "1"
+	metaPool.Put(m)
+}
+
+// JSON returns the `Meta` JSON encoded as (hash, js)
+func (m *Meta) Json() (string, []byte) {
+	js, err := json.Marshal(m)
+	if err != nil {
+		panic(err)
+	}
+	h := fmt.Sprintf("%x", blake2b.Sum256(js))
+	return h, js
+}
+
+func (m *Meta) AddIndexedRef(index int, hash string) {
+	m.Refs = append(m.Refs, []interface{}{index, hash})
+}
+
+func (m *Meta) AddRef(hash string) {
+	m.Refs = append(m.Refs, hash)
+}
+
+func NewMeta() *Meta {
+	return metaPool.Get().(*Meta)
+}
+
+func NewMetaFromBlobStore(hash string, blob []byte) (*Meta, error) {
+	meta := NewMeta()
+	if err := json.Unmarshal(blob, meta); err != nil {
+		return nil, err
+	}
+	meta.Hash = hash
+	return meta, nil
+}
+
+func (m *Meta) ContentType() string {
+	if m.IsFile() {
+		return mime.TypeByExtension(filepath.Ext(m.Name))
+	}
+	return ""
+}
+
+// IsFile returns true if the Meta is a file.
+func (m *Meta) IsFile() bool {
+	if m.Type == "file" {
+		return true
+	}
+	return false
+}
+
+// IsDir returns true if the Meta is a directory.
+func (m *Meta) IsDir() bool {
+	if m.Type == "dir" {
+		return true
+	}
+	return false
+}
+
+func (m *Meta) Close() {
+	m.free()
+}
---
+++ ext/filetree/writer/uploader.go
@@ -0,0 +1,58 @@
+package writer
+
+import (
+	"github.com/tsileo/blobstash/client/blobstore"
+	"github.com/tsileo/blobstash/client/interface"
+)
+
+var (
+	uploader    = 25 // concurrent upload uploaders
+	dirUploader = 12 // concurrent directory uploaders
+)
+
+type Uploader struct {
+	bs *blobstore.BlobStore
+
+	uploader    chan struct{}
+	dirUploader chan struct{}
+
+	// Ignorer *gignore.GitIgnore
+	Root string
+}
+
+func NewUploader(bs *blobstore.BlobStore) *Uploader {
+	return &Uploader{
+		bs: bs,
+		// kvs:         kvs,
+		uploader:    make(chan struct{}, uploader),
+		dirUploader: make(chan struct{}, dirUploader),
+	}
+}
+
+// Block until the client can start the upload, thus limiting the number of file descriptor used.
+func (up *Uploader) StartUpload() {
+	up.uploader <- struct{}{}
+}
+
+// Read from the channel to let another upload start
+func (up *Uploader) UploadDone() {
+	select {
+	case <-up.uploader:
+	default:
+		panic("No upload to wait for")
+	}
+}
+
+// Block until the client can start the upload, thus limiting the number of file descriptor used.
+func (up *Uploader) StartDirUpload() {
+	up.dirUploader <- struct{}{}
+}
+
+// Read from the channel to let another upload start
+func (up *Uploader) DirUploadDone() {
+	select {
+	case <-up.dirUploader:
+	default:
+		panic("No upload to wait for")
+	}
+}
