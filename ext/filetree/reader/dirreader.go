f50a70b30e476c3364e205c431c53ee06a9e5acb
Thomas Sileo <thomas.sileo@gmail.com>
2016-01-31 14:58:51 +0100

    ext/filetree: added dirreader

---
+++ ext/filetree/reader/dirreader.go
@@ -0,0 +1,58 @@
+package reader
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	_ "github.com/dchest/blake2b"
+
+	"github.com/tsileo/blobstash/client/blobstore"
+	"github.com/tsileo/blobstash/ext/filetree/filetreeutil/meta"
+	"github.com/tsileo/blobstash/ext/filetree/reader/filereader"
+)
+
+// GetDir restore the directory to path
+func GetDir(bs *blobstore.BlobStore, hash, path string) error { // (rr *ReadResult, err error) {
+	// fullHash := blake2b.New256()
+	// rr = &ReadResult{}
+	if err := os.Mkdir(path, 0700); err != nil {
+		return err
+	}
+
+	js, err := bs.Get(hash)
+	if err != nil {
+		return err
+	}
+	cmeta, err := meta.NewMetaFromBlob(hash, js)
+	if err != nil {
+		return fmt.Errorf("failed to fetch meta %s: %v", hash, err)
+	}
+	cmeta.Hash = hash
+	// var crr *ReadResult
+	if cmeta.Size > 0 {
+		for _, hash := range cmeta.Refs {
+			blob, err := bs.Get(hash.(string))
+			submeta, err := meta.NewMetaFromBlob(hash.(string), blob)
+			if err != nil {
+				return fmt.Errorf("failed to fetch meta: %v", err)
+			}
+			if submeta.IsFile() {
+				if err := filereader.GetFile(bs, submeta.Hash, filepath.Join(path, submeta.Name)); err != nil {
+					return fmt.Errorf("failed to GetFile %+v: %v", submeta, err)
+				}
+			} else {
+				if err := GetDir(bs, submeta.Hash, filepath.Join(path, submeta.Name)); err != nil {
+					return fmt.Errorf("failed to GetDir %+v: %v", submeta, err)
+				}
+			}
+			// fullHash.Write([]byte(crr.Hash))
+			// rr.Add(crr)
+		}
+	}
+	// TODO(tsileo): sum the hash and check with the root
+	// rr.DirsCount++
+	// rr.DirsDownloaded++
+	// rr.Hash = fmt.Sprintf("%x", fullHash.Sum(nil))
+	return nil
+}
