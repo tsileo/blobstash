e321a6b9be6de228039e72afcc2a8829d9dc242a
Thomas Sileo <thomas.sileo@gmail.com>
2016-02-02 23:58:26 +0100

    httputil: updated request logger (added resp_time)

--- httputil/logger.go
+++ httputil/logger.go
@@ -2,6 +2,7 @@ package httputil
 
 import (
 	"net/http"
+	"time"
 
 	log "gopkg.in/inconshreveable/log15.v2"
 )
@@ -9,8 +10,10 @@ import (
 func LoggerMiddleware(logger log.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
 			next.ServeHTTP(w, r)
-			log.Info(r.URL.String(), "method", r.Method, "len", r.ContentLength, "proto", r.Proto, "ip", GetIpAddress(r))
+			log.Info(r.URL.String(), "method", r.Method, "len", r.ContentLength, "proto", r.Proto,
+				"resp_time", time.Since(start), "ip", GetIpAddress(r))
 		})
 	}
 }
