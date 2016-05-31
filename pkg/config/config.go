6575f0edd8211357bffe2b4e70c0d21cb13dacb6
Thomas Sileo <thomas.sileo@gmail.com>
2016-05-31 23:44:58 +0200

    config: started new YAML config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,7 +1,23 @@
 package config
 
 import (
-	_ "gopkg.in/yaml.v2"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
 )
 
-type Config struct{}
+type Config struct {
+	Listen string `yaml:"listen"`
+}
+
+func New(path string) (*Config, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	conf := &Config{}
+	if err := yaml.Unmarshal([]byte(data), &conf); err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
