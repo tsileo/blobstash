df9947ce51833b0e8e0bba2b62064d4f33eb669e
Thomas Sileo <thomas.sileo@gmail.com>
2016-06-19 21:34:38 +0200

    httputil: add support for passing API key via query arg

--- pkg/httputil/auth.go
+++ pkg/httputil/auth.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"crypto/subtle"
 	"encoding/base64"
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -15,20 +16,26 @@ var BasicRealm = "Authorization Required"
 
 func BasicAuthFunc(username string, password string) func(*http.Request) bool {
 	return func(req *http.Request) bool {
+		fmt.Printf("\n\nINSIDE BASICAUTH %+v\n\n", req)
 		auth := req.Header.Get("Authorization")
 		switch {
 		case strings.HasPrefix(auth, "Basic "):
 			siteAuth := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
-			if !secureCompare(auth, "Basic "+siteAuth) {
-				return false
+			if secureCompare(auth, "Basic "+siteAuth) {
+				return true
 			}
-			return true
+			return false
+		case req.URL.Query().Get("api_key") != "":
+			fmt.Printf("\n\nAPI_KE %v %v\n\n", req.URL.Query().Get("api_key"), password)
+			if secureCompare(req.URL.Query().Get("api_key"), password) {
+				return true
+			}
+			return false
 		case strings.HasPrefix(auth, "key "):
-			if !secureCompare(auth, "key "+password) {
-				return false
+			if secureCompare(auth, "key "+password) {
+				return true
 			}
-			return true
-
+			return false
 		}
 		return false
 	}
