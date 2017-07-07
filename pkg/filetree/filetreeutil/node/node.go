2ddb930bae8e996400102fd34ceecdcd50c53c88
Thomas Sileo <t@a4.io>
2017-07-07 23:47:51 +0200

    filetree: add missing pkg

---
+++ pkg/filetree/filetreeutil/node/node.go
@@ -0,0 +1,79 @@
+/*
+
+Package node implement the node specification for the filetree extension.
+
+*/
+package node // import "a4.io/blobstash/pkg/filetree/filetreeutil/node"
+
+import (
+	"fmt"
+	"mime"
+	"path/filepath"
+
+	"github.com/dchest/blake2b"
+	"github.com/vmihailenco/msgpack"
+)
+
+var (
+	version = "1"
+)
+
+type RawNode struct {
+	Name     string                 `msgpack:"n"`
+	Type     string                 `msgpack:"t"`
+	Size     int                    `msgpack:"s"`
+	Refs     []interface{}          `msgpack:"r"`
+	Version  string                 `msgpack:"v"`
+	Metadata map[string]interface{} `msgpack:"m,omitempty"`
+	Hash     string                 `msgpack:"-"`
+}
+
+// AddData insert a new meta data in the Data field
+func (n *RawNode) AddData(key string, val interface{}) {
+	if n.Metadata == nil {
+		n.Metadata = map[string]interface{}{}
+	}
+	n.Metadata[key] = val
+}
+
+// JSON returns the `Node` encoded as (hash, blob)
+func (n *RawNode) Encode() (string, []byte) {
+	js, err := msgpack.Marshal(n)
+	if err != nil {
+		panic(err)
+	}
+	h := fmt.Sprintf("%x", blake2b.Sum256(js))
+	return h, js
+}
+
+func (n *RawNode) AddIndexedRef(index int, hash string) {
+	n.Refs = append(n.Refs, []interface{}{index, hash})
+}
+
+func (n *RawNode) AddRef(hash string) {
+	n.Refs = append(n.Refs, hash)
+}
+
+func NewNodeFromBlob(hash string, blob []byte) (*RawNode, error) {
+	node := &RawNode{}
+	if err := msgpack.Unmarshal(blob, node); err != nil {
+		return nil, err
+	}
+	node.Hash = hash
+	return node, nil
+}
+
+func (n *RawNode) ContentType() string {
+	if n.IsFile() {
+		return mime.TypeByExtension(filepath.Ext(n.Name))
+	}
+	return ""
+}
+
+// IsFile returns true if the Meta is a file.
+func (n *RawNode) IsFile() bool {
+	if n.Type == "file" {
+		return true
+	}
+	return false
+}
