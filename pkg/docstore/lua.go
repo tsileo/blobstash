77681f3d5279ac2c92da3f68b1e1eb67c9a19e20
Thomas Sileo <t@a4.io>
2017-08-10 01:41:41 +0200

    docstore: add get_path and in_list Lua helpers

--- pkg/docstore/lua.go
+++ pkg/docstore/lua.go
@@ -69,13 +69,64 @@ func (docstore *DocStore) newLuaQueryEngine(query *query) (*LuaQueryEngine, erro
 	gluarequire2.NewRequire2Module(gluarequire2.NewRequireFromGitHub(nil)).SetGlobal(engine.L)
 	setGlobals(engine.L)
 	if err := engine.L.DoString(`
-return function (t, wanted)
-  print('in_list')
-  print(t)
-  print(wanted)
-  for _, value in ipairs(t) do
-    if value == wanted then
-      return true
+-- Python-like string.split implementation http://lua-users.org/wiki/SplitJoin
+function string:split(sSeparator, nMax, bRegexp)
+   assert(sSeparator ~= '')
+   assert(nMax == nil or nMax >= 1)
+
+   local aRecord = {}
+
+   if self:len() > 0 then
+      local bPlain = not bRegexp
+      nMax = nMax or -1
+
+      local nField, nStart = 1, 1
+      local nFirst,nLast = self:find(sSeparator, nStart, bPlain)
+      while nFirst and nMax ~= 0 do
+         aRecord[nField] = self:sub(nStart, nFirst-1)
+         nField = nField+1
+         nStart = nLast+1
+         nFirst,nLast = self:find(sSeparator, nStart, bPlain)
+         nMax = nMax-1
+      end
+      aRecord[nField] = self:sub(nStart)
+   end
+
+   return aRecord
+end
+function get_path (doc, q)
+  q = q:gsub('%[%d', '.%1')
+  local parts = q:split('.')
+  p = doc
+  for _, part in ipairs(parts) do
+    if type(p) ~= 'table' then
+      return nil
+    end
+    if part:sub(1, 1) == '[' then
+      part = part:sub(2, 2)
+    end
+    if tonumber(part) ~= nil then
+      p = p[tonumber(part)+1]
+    else
+      p = p[part]
+    end
+    if p == nil then
+      return nil
+    end
+  end
+  return p
+end
+_G.get_path = get_path
+return function (doc, path, value, q)
+  local p = get_path(doc, path)
+  if type(p) ~= 'table' then
+    return false
+  end
+  for _, item in ipairs(p) do
+    if q == nil then
+      if item == value then return true end
+    else
+      if get_path(item, q) == value then return true end
     end
   end
   return false
@@ -84,6 +135,7 @@ end
 		panic(err)
 	}
 	engine.L.SetGlobal("in_list", engine.L.Get(-1).(*lua.LFunction))
+	engine.L.Pop(1)
 
 	engine.logger.Debug("init", "query", engine.query)
 	// Parse the Lua query, which should be defined as a `function(doc) -> bool`, we parse it only once, then we got
@@ -122,7 +174,8 @@ end
 				NRet:    1,
 				Protect: true,
 			}, luautil.InterfaceToLValue(engine.L, doc)); err != nil {
-				return false, err
+				fmt.Printf("failed to call match func: %+v %+v\n", doc, err)
+				return false, err // FIXME(tsileo): a way to switch the return error/don't return error?
 			}
 			ret := engine.L.Get(-1)
 			engine.L.Pop(1)
