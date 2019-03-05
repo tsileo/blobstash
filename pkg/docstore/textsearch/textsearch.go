e6a6d6a4963b0999b0eca4165221c202a8c8a5b2
Thomas Sileo <t@a4.io>
2019-03-05 23:06:01 +0100

    docstore/textsearch: update docs

--- pkg/docstore/textsearch/textsearch.go
+++ pkg/docstore/textsearch/textsearch.go
@@ -1,3 +1,6 @@
+/*
+Package textsearch implements basic text search features (for matching text fields of JSON documents).
+*/
 package textsearch // import "a4.io/blobstash/pkg/textsearch"
 
 import (
@@ -11,21 +14,26 @@ import (
 	porterstemmer "github.com/reiver/go-porterstemmer"
 )
 
-var cache, _ = lru.New(128)
+// cache for `string` -> `SearchTerms`
+var searchTermsCache, _ = lru.New(128)
 
+// searchTerm holds a single search term
 type searchTerm struct {
-	prefix     string
+	prefix     string // `+` (for required match) or `-` (for excluding doc matching the term)
 	term       string
-	exactMatch bool
+	exactMatch bool // true if the search term was quoted for exact match
 }
 
+// SearchTerms holds a parsed text search query
 type SearchTerms []*searchTerm
 
+// IndexedDoc holds a parsed "document"
 type IndexedDoc struct {
 	Content string         `msgpack:"c"`
 	Stems   map[string]int `msgpack:"s"`
 }
 
+// NewIndexedDoc returns a parsed "document"
 func NewIndexedDoc(doc map[string]interface{}, fields []string) (*IndexedDoc, error) {
 	parts := []string{}
 	stems := map[string]int{}
@@ -53,38 +61,7 @@ func NewIndexedDoc(doc map[string]interface{}, fields []string) (*IndexedDoc, er
 	return &IndexedDoc{Content: content, Stems: stems}, nil
 }
 
-func (terms SearchTerms) Match(d *IndexedDoc) bool {
-	match := false
-
-	for _, st := range terms {
-		cond := false
-		switch {
-		case st.exactMatch:
-			cond = strings.Contains(d.Content, st.term)
-		default:
-			_, cond = d.Stems[st.term]
-		}
-
-		if st.prefix == "+" {
-			if !cond {
-				return false
-			}
-		} else if st.prefix == "-" {
-			if cond {
-				return false
-			} else {
-				match = true
-			}
-		}
-
-		if cond {
-			match = true
-		}
-	}
-
-	return match
-}
-
+// ParseTextQuery returns a parsed text query
 func ParseTextQuery(q string) SearchTerms {
 	if cached, ok := cache.Get(q); ok {
 		fmt.Printf("ParseTextQuery form cache")
@@ -123,4 +100,37 @@ func ParseTextQuery(q string) SearchTerms {
 	}
 	cache.Add(q, out)
 	return out
+}
+
+// Match returns true if the query matches the given `IndexedDoc`
+func (terms SearchTerms) Match(d *IndexedDoc) bool {
+	match := false
+
+	for _, st := range terms {
+		cond := false
+		switch {
+		case st.exactMatch:
+			cond = strings.Contains(d.Content, st.term)
+		default:
+			_, cond = d.Stems[st.term]
+		}
+
+		if st.prefix == "+" {
+			if !cond {
+				return false
+			}
+		} else if st.prefix == "-" {
+			if cond {
+				return false
+			} else {
+				match = true
+			}
+		}
+
+		if cond {
+			match = true
+		}
+	}
+
+	return match
 }
